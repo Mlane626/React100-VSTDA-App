5aecbd939d884f462dac8674c34c735d17872f58
Mike L <mike.lane.626@gmail.com>
2023-04-21 11:39:40 -0700

    Adds card , changed version of bootstrap to 5.3.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
-
   render() {
-    const hStyle = { color: 'white' };
-    const hRow = { backgroundColor: 'White', height: '5px' };
+    const hStyle = { color: "white" };
+    const hRow = { backgroundColor: "White", height: "5px" };
 
     return (
-      <div className='container'>
+      <div className="container">
         <h1>Very Simple To Do App</h1>
         <h2 style={hStyle}>Track all of the things</h2>
         <hr style={hRow} />
-        <header>
-          <div className="row">
-            <div className="col-4">
-              <div className="card">
-                <div className="card-header" style={{ backgroundColor: "#a9a9a9" }}>
-                </div>
-              </div>
-            </div>
+        {/* <header> */}
+        <div className="card" style={{width: 24 + 'rem'}}>
+          <div className="card-header">Featured</div>
+          <div className="card-body">
+            <h5 className="card-title">I Want To...</h5>
+            <input>
+            </input>
+            <h5 className="card-title">How Much Of A Priority is This?</h5>
+            <select>
+              Select A Priority
+            </select>
+            <a href="#" className="btn btn-primary">
+              Add
+            </a>
           </div>
-        </header>
+        </div>
       </div>
     );
   }
